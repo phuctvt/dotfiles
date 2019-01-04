a63d22602f01e77b63f3100dd6d04b1466002d09
leo <leo@leo.com>
2019-01-04 20:59:14 +0700

    Update

--- get-volume.sh
+++ get-volume.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 
+# https://bbs.archlinux.org/viewtopic.php?id=227521
+
+x=$(pacmd list-cards | grep -P "analog-output-headphones.+?available" | awk -F ":" '{print $3}')
+speaker_icon=""
+if [ "$x" = " yes)" ]; then
+	speaker_icon=""
+fi
+
 if [ $(pulsemixer --get-mute) = 1 ]; then
 	echo ""
 else
-	pulsemixer --get-volume | awk '{print " " $1}'
+	echo -n "$speaker_icon"
+	pulsemixer --get-volume | awk '{print " " $1}'
 fi
