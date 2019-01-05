5154a5aaa461079852937593fcf119ec9b5d1349
leo <leo@leo.com>
2019-01-05 22:05:43 +0700

    Update

--- scripts/change-volume.sh
+++ scripts/change-volume.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "up" ]; then
 	pulsemixer --change-volume +1
-	play ~/dotfiles/scripts/button-09.wav
+	# play ~/dotfiles/scripts/Blooper-sound-effect.wav
 elif [ "$1" = "down" ]; then
 	pulsemixer --change-volume -1
-	play ~/dotfiles/scripts/button-09.wav
+	# play ~/dotfiles/scripts/Blooper-sound-effect.wav
 elif [ "$1" = "toggle-mute" ]; then
 	pulsemixer --toggle-mute
 fi
