d2617ae6fa2b3e91c78412976a4350cd306f6660
leo <leo@leo.com>
2019-01-18 21:29:14 +0700

    Update

--- scripts/gnome-startup.sh
+++ scripts/gnome-startup.sh
@@ -1,4 +1,5 @@
-xinput set-prop "ELAN0501:00 04F3:300B Touchpad" 303 1
-xinput set-prop "ELAN0501:00 04F3:300B Touchpad" 285 1
+# xinput set-prop "ELAN0501:00 04F3:300B Touchpad" 303 1
+# xinput set-prop "ELAN0501:00 04F3:300B Touchpad" 285 1
+/usr/bin/python2 ~/.dropbox-dist/dropbox.py start
 notify-send "GNOME Startup Script" "Finish!"
 
